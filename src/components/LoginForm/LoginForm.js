8c1a742e58262601ed42530fa11619971474be98
Ryan Whitmore <ryan@whitmorespace.com>
2020-08-28 11:42:44 -0700

    added auth-api-services file

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import { Button, Input } from '../Utils/Utils'
-import TokenService from '../../services/token-service'
+import React, { Component } from 'react';
+import { Button, Input } from '../Utils/Utils';
+import TokenService from '../../services/token-service';
+import AuthApiService from '../../services/auth-api-service';
 
 export default class LoginForm extends Component {
   static defaultProps = {
@@ -10,24 +11,44 @@ export default class LoginForm extends Component {
   state = { error: null }
 
   handleSubmitBasicAuth = ev => {
-    ev.preventDefault()
-    const { user_name, password } = ev.target
+    ev.preventDefault();
+    const { user_name, password } = ev.target;
 
     TokenService.saveAuthToken(
       TokenService.makeBasicAuthToken(user_name.value, password.value)
-    )
+    );
 
-    user_name.value = ''
-    password.value = ''
-    this.props.onLoginSuccess()
+    user_name.value = '';
+    password.value = '';
+    this.props.onLoginSuccess();
+  }
+
+  handleSubmitJwtAuth = ev => {
+    ev.preventDefault();
+    this.setState({ error: null });
+    const { user_name, password } = ev.target;
+
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value
+    })
+      .then(res => {
+        user_name.value = '';
+        password.value = '';
+        TokenService.saveAuthToken(res.authToken);
+        this.props.onLoginSuccess();
+      })
+      .catch(res => {
+        this.setState({ error: res.error });
+      });
   }
 
   render() {
-    const { error } = this.state
+    const { error } = this.state;
     return (
       <form
         className='LoginForm'
-        onSubmit={this.handleSubmitBasicAuth}
+        onSubmit={this.handleSubmitJwtAuth}
       >
         <div role='alert'>
           {error && <p className='red'>{error}</p>}
@@ -57,6 +78,6 @@ export default class LoginForm extends Component {
           Login
         </Button>
       </form>
-    )
+    );
   }
 }
